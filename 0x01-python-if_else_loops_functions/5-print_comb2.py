5bc432f5c715b1a90d939ada6db6f0d3dcdbcf2d
danjor667 <jordannguepi@gmail.com>
2023-06-06 05:50:49 -0700

    task 5v2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i == 9 and j == 9:
-            break
-        else:
-            print("{}{}".format(i, j), end=", ")
-print("{}{}".format(i, j))
+for i in range(100):
+    if i == 99:
+        break
+    print("{}{}".format(i // 10, i % 10), end=", ")
+print("{}{}".format(i // 10, i % 10))
