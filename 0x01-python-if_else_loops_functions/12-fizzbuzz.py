c4da2f3d6f7f2b6b6779123bd80d40ec551d7e3f
danjor667 <jordannguepi@gmail.com>
2023-06-06 15:03:39 -0700

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i%3 == False and i%5 == False:
+        if i % 3 == 0 and i % 5 == 0:
             s = "FizzBuzz"
-        elif i%3 == False:
+        elif i % 3 == 0:
             s = "Fizz"
-        elif i%5 == False:
+        elif i % 5 == 0:
             s = "Buzz"
         else:
             s = str(i)
