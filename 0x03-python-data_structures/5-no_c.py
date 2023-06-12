de3172312478be6a0324054101dbd7e06cdba820
danjor667 <jordannguepi@gmail.com>
2023-06-12 05:05:20 -0700

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    str = ""
+    for i in range(len(my_string)):
+        if my_string[i] != "c" or my_string[i] != "C":
+            str += my_string[i]
+    return str
