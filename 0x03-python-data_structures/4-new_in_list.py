b0c0eda0085da064f2f15aa7696c11e2406b9647
danjor667 <jordannguepi@gmail.com>
2023-06-12 04:56:52 -0700

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,7 @@ def new_in_list(my_list, idx, element):
         return my_list
     for i in range(len(my_list)):
         if i == idx:
-            new.append(my_list[idx])
+            new.append(element)
 
         else:
             new.append(my_list[i])
