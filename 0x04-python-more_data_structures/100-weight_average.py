0979d16d935931a8423b174fcbaf80915903fa4e
danjor667 <jordannguepi@gmail.com>
2023-06-14 07:04:10 -0700

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,12 @@
 
 
 def weight_average(my_list=[]):
-    up= 0
-    down = 0
-    for key, value in dict(my_list).items():
-        up += (key * value)
-        down += value
-    return up / down
+    if my_list:
+        up = 0
+        down = 0
+        for key, value in dict(my_list).items():
+            up += (key * value)
+            down += value
+        return up / down
+    else:
+        return 0
