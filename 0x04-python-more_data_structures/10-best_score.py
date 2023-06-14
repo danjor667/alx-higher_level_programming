b2828a5cfb8e665a823e88124d83cc8c8d39d8c8
danjor667 <jordannguepi@gmail.com>
2023-06-14 05:46:01 -0700

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+
+
+def best_score(a_dictionary):
+    maximum = 0
+    name = ""
+    for key, value in a_dictionary.items():
+        if value > maximum:
+            maximum = value
+            name = key
+        return name
