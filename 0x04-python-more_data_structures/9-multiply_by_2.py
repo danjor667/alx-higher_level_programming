52fd3edc6c3ec4976b05630faf299b49f28a1969
danjor667 <jordannguepi@gmail.com>
2023-06-14 05:33:10 -0700

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,12 @@
 
 
 def multiply_by_2(a_dictionary):
-    return dict([keys: value * 2 for items in a_dictionary.items()])
+    return {key: value * 2 for key, value in a_dictionary.items()}
+
+
+"""a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+new_dict = multiply_by_2(a_dictionary)
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+"""
