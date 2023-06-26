945ec6789a60fb18e4274bcdab9a6b972e83fdcd
danjor667 <jordannguepi@gmail.com>
2023-06-26 14:33:11 -0700

    task0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    for i < x:
+        try:
+            print("{}".format(my_list[i]), end="")
+        except:
+            break
+        n += 1
+    print("")
+    return (n)
