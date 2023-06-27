19fe913bc517a6f5e401d0443a31194c47c4d145
danjor667 <jordannguepi@gmail.com>
2023-06-27 16:53:10 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Class Square''' 
+  
+  
+class Square:
+    '''Instance of Square'''
+    def __init__(self, size=0):
+        '''Initializes instances of Square.
+        Args:size (int): Represents the size of one side of the square.'''
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
