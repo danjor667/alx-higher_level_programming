1d150f9f600fd18f34549184ab9f096d0d94799f
danjor667 <jordannguepi@gmail.com>
2023-06-27 12:19:28 -0700

    task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as te:
+        stderr.write("Exception: {}".format(te))
+        return False
+    return True
