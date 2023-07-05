fe961f61fa177b8a4668f654d908d317c861a733
danjor667 <jordannguepi@gmail.com>
2023-07-05 15:58:01 -0700

    task 6 retry

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,8 @@ class Rectangle:
     """
     intialising an instance
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ init new instances
             Args:
