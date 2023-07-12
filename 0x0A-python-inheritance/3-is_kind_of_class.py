9c963aa64f5c2c8627c0f13b34ac0a0cc2b59479
danjor667 <jordannguepi@gnail.com>
2023-07-12 13:17:53 +0000

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+funct, is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    a funct to verify is and instance is an obj of a class
+    Arg:
+        obj
+        a_class
+    Return:
+        bool
+    """
+    return (isinstance(obj, a_class))
