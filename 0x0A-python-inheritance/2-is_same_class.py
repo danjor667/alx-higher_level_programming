0aeb61c76633688419ca0792fad4b722ae463609
danjor667 <jordannguepi@gnail.com>
2023-07-12 12:37:51 +0000

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def is_same_class(obj, a_class):
+    """
+    function to verify if obj is an instance of a_class
+    Arg:
+        obj
+    Returns:
+        bool
+    """
+    return (isinstance(obj, a_class))
