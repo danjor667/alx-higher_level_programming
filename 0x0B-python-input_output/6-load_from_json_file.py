373b87ceca7041aa5dc88296ae5a4f4bdfa54770
danjor667 <jordannguepi@gnail.com>
2023-07-13 17:07:29 +0000

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+defines function load_from_json_file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creats an obj fron a json file
+    Args:
+        filename: file to;read from
+    Return:
+        python obj
+    """
+    with open(filename) as f:
+        obj = json.load(file)
+        return obj
