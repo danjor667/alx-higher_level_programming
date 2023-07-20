ba69429f54137ae98ea182e0c69572587f923b89
danjor667 <jordannguepi@gmail.com>
2023-07-20 15:24:02 -0700

    clean up

--- 0x0C-python-almost_a_circle/main/1-main.py
+++ 0x0C-python-almost_a_circle/main/1-main.py
@@ -5,10 +5,13 @@ from models.rectangle import Rectangle
 if __name__ == "__main__":
 
     r1 = Rectangle(10, 2)
+    print("success")
     print(r1.id)
 
     r2 = Rectangle(2, 10)
+    print("success")
     print(r2.id)
 
     r3 = Rectangle(10, 2, 0, 0, 12)
+    print("sucess")
     print(r3.id)
