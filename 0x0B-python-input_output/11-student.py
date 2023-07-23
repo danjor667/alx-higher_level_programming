dc2b80b2c6b8235acd2ce899ea6aad54ee5baa6f
danjor667 <jordannguepi@gnail.com>
2023-07-23 23:45:53 +0000

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+defining a class Stident
+"""
+
+
+class Student():
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialising an instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns a dict repr of
+        an instance
+        """
+        my_dict = self.__dict__
+        if attrs is None:
+            return my_dict
+        elif isinstance(attrs, list):
+            new_dict = {}
+            for key, value in my_dict.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
+
+    def reload_from_json(self, json):
+        """
+        reloads data a dict an updates an instance
+        attributes
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
