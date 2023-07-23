dafb9ebf64a2a614319db181b9a33c49f310de2b
danjor667 <jordannguepi@gnail.com>
2023-07-23 22:55:36 +0000

    task 7 update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -17,6 +17,10 @@ def to_list():
     except FileNotFoundError:
         with open("add_item.json", "w") as f:
             f.write("[]")
+        my_list = load_from_json_file("add_item.json")
+        for i in range(1, len(sys.argv)):
+            my_list.append(sys.argv[i])
+        save_to_json_file(my_list, "add_item.json")
 
 
 if __name__ == "__main__":
