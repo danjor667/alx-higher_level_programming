7bd439495c7a0b42a288b16eb2e237f1dad7d05d
danjor667 <jordannguepi@gnail.com>
2023-07-23 23:19:01 +0000

    task 9 update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,7 +13,7 @@ class Student():
         initialising an instance
         """
         self.first_name = first_name
-        self.last_name last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
