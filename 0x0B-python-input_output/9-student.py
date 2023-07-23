0847048f9b0003a47ee3088e8ab5dae40debe4cf
danjor667 <jordannguepi@gnail.com>
2023-07-23 23:11:34 +0000

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+defining a class Stident
+"""
+
+
+class Student():
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name age):
+        """
+        initialising an instance
+        """
+        self.first_name = first_name
+        self.last_name last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        returns a dict repr of
+        an instance
+        """
+        return self.__dict__
