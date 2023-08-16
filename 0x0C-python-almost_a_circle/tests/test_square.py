3f3907b2989d939c62413db3d8a86ff83db5366b
danjor667 <jordannguepi@gmail.com>
2023-08-16 05:14:17 -0700

    square test v2

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -5,7 +5,7 @@ testing rectangle
 
 import unittest
 import json
-from models.rectangle import Rectangle
+#from models.rectangle import Rectangle
 from models.square import Square
 
 class TestSquare(unittest.TestCase):
@@ -20,8 +20,8 @@ class TestSquare(unittest.TestCase):
         r2 = Square(1,2)
         r3 = Square(1,2,3)
         self.assertEqual(r1.size, 1)
-        self.assertEqual(r1.x, 2)
-        self.assertEqual(r2.y, 3)
+        self.assertEqual(r2.x, 2)
+        self.assertEqual(r3.y, 3)
         #self.assertEqual(r3.y, 4)
 
     def test_square_args(self):
@@ -36,7 +36,7 @@ class TestSquare(unittest.TestCase):
         self.assertRaises(ValueError, Square, 1, -3,4)
         self.assertRaises(ValueError, Square, -2,3,4)
         #self.assertRaises(ValueError,Rectangle, -1,2,3,4)
-        self.assertEqual(Squ(1,2,3,5).id, 5)
+        self.assertEqual(Square(1,2,3,5).id, 5)
         self.assertRaises(ValueError, Square, 0, )
         #self.assertRaises(ValueError, Rectangle,1, 0)
 
@@ -46,13 +46,13 @@ class TestSquare(unittest.TestCase):
         """
         r2 = Square(2, id=24)
         self.assertEqual(r2.area(), 4)
-        self.assertEqual(r2.__str__(), "[Square] (24) 0/0 - 2/2")
+        self.assertEqual(r2.__str__(), "[Square] (24) 0/0 - 2")
 
     def test_methods_update(self):
         """
         doc
         """
-        r = Rectangle(10, id=12)
+        r = Square(10, id=12)
         r.update(1)
         self.assertNotEqual(r.id, 12)
         r.update(1,2)
@@ -84,7 +84,7 @@ class TestSquare(unittest.TestCase):
         """
         r = Square.create(**{"id": 23})
         self.assertEqual(r.id, 23)
-        r = Rectangle.create(**{"id": 23, "size": 2})
+        r = Square.create(**{"id": 23, "size": 2})
         self.assertEqual(r.size, 2)
         #r = Rectangle.create(**{"id": 23, "width": 2, "height": 3})
         #self.assertEqual(r.height, 3)
@@ -97,7 +97,7 @@ class TestSquare(unittest.TestCase):
         """
         doc
         """
-        r = Square(1,2, id=23)
+        r = Square(1, id=23)
         Square.save_to_file(None)
         with open("Square.json") as f:
             cont = f.read()
@@ -126,5 +126,5 @@ class TestSquare(unittest.TestCase):
         r = Square(2)
         Square.save_to_file([r])
         my_list = Square.load_from_file()
-        self.assertEqual(type(my_list[0]), type(Rectangle.create(**r.to_dictionary())))
+        self.assertEqual(type(my_list[0]), type(Square.create(**r.to_dictionary())))
 
