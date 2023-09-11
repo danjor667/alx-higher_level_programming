300585363933c0db135fc6695e42b34c3863c808
danjor667 <jordannguepi@gmail.com>
2023-09-11 12:22:39 +0200

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,11 +5,10 @@ if (!isNaN(parseInt(process.argv[2]))) {
   size = parseInt(process.argv[2]);
   if (size > 0) {
     const figure = 'X'.repeat(size);
-    for (let h = 0; h <= size; h++) {
+    for (let h = 0; h < size; h++) {
       console.log(figure);
     }
   }
 } else {
   console.log('Missing size');
 }
-
