948918fdbd983e86be612b01271cdc01cf380d5d
danjor667 <jordannguepi@gmail.com>
2023-09-11 12:24:27 +0200

    fix:task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let i;
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurences');
 } else {
-  for (i = 0; i <= parseInt(process.argv[2]); i++) {
+  for (i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
   }
 }
