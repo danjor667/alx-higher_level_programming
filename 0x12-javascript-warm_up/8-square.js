a9855a3a07cfb26a1748de1f587621d98516ce19
danjor667 <jordannguepi@gmail.com>
2023-09-11 12:16:39 +0200

    fix: task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,13 @@
 let size;
 if (!isNaN(parseInt(process.argv[2]))) {
   size = parseInt(process.argv[2]);
-  const figure = 'X'.repeat(size);
-  for (let h = 0; h <= size; h++) {
-    console.log(figure);
+  if (size > 0) {
+    const figure = 'X'.repeat(size);
+    for (let h = 0; h <= size; h++) {
+      console.log(figure);
+    }
   }
 } else {
   console.log('Missing size');
 }
+
