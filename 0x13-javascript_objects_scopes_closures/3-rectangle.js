a7b5f5459195fae23925dc20c89701f9caedacbf
danjor667 <jordannguepi@gmail.com>
2023-09-13 19:11:20 +0200

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((typeof w) !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
+      // pass
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    const fig = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(fig);
+    }
+  }
+}
+module.exports = Rectangle;
