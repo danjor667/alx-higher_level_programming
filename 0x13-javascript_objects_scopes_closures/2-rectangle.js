666d7f12b613cddb45dcf11b6f50fe13f776878a
danjor667 <jordannguepi@gmail.com>
2023-09-13 18:57:21 +0200

    fix task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if ((typeof w) !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
-      this.width = undefined;
-      this.height = undefined;
+      ;
     } else {
       this.width = w;
       this.height = h;
