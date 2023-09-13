38aa10828cd3a3d3d6879ceab40c6d8910906ec4
danjor667 <jordannguepi@gmail.com>
2023-09-13 19:01:28 +0200

    fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,8 @@
 class Rectangle {
   constructor (w, h) {
     if ((typeof w) !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
-      ;
+      // pass
+
     } else {
       this.width = w;
       this.height = h;
