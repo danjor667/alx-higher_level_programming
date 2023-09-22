56f6d2cc74b5e41d8382ba11de254f9345ae95dd
danjor667 <jordannguepi@gmail.com>
2023-09-22 21:56:19 +0200

    fix

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+doc
+"""
+
+from model_city import City
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3],
+            pool_pre_ping=True)
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(City).order_by(City.id).all()
+    for city in cities:
+        state = session.query(State).filter(City.state_id == State.id).first()
+        print(f"{state.name}: ({city.id}) {city.name}")
