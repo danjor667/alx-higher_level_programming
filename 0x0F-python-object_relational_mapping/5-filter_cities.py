faa9079a67533b5b42babf0be46d5b0c8574ad56
danjor667 <jordannguepi@gmail.com>
2023-09-22 10:12:07 +0200

    fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     query = "SELECT cities.name\
              FROM cities INNER JOIN states\
              ON cities.state_id = states.id\
-             WHERE states.name = %s"
+             WHERE states.name = %s\
+             ORDER BY cities.id ASC"
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
