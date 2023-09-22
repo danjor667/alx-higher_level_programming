ba52f5db1f907a0c9c4f5dbf194318b9d7249fa9
danjor667 <jordannguepi@gmail.com>
2023-09-22 12:14:30 +0200

    fix 2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,7 +14,7 @@ class State(Base):
     """
     doc
     """
-    __tablename__ == "states"
+    __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
